2e98c2a6ca3a02527ffc0090903da6bd702590bd
Victor Ferreira <82904762+victorfyonemoto@users.noreply.github.com>
2022-05-03 14:56:37 -0300

    Delete Java/desafioBanco directory

--- Java/desafioBanco/src/iConta.java
+++
@@ -1,6 +0,0 @@
-public interface iConta {
-    void sacar(double valor);
-    void depositar(double valor);
-    void transferir(double valor, conta contaDestino);
-    void imprimirExtrato();
-    }
