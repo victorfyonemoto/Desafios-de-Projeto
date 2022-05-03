d395f4c4709896d7a64a1387a7146b766eb2fd17
Victor Ferreira <82904762+victorfyonemoto@users.noreply.github.com>
2022-05-03 14:57:48 -0300

    Add files via upload
    first commit

---
+++ Java/desafioBanco/src/iConta.java
@@ -0,0 +1,6 @@
+public interface iConta {
+    void sacar(double valor);
+    void depositar(double valor);
+    void transferir(double valor, conta contaDestino);
+    void imprimirExtrato();
+    }
