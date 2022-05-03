06d5fab1c32b4d3f52f3ab99289195d20e5490e7
Victor Ferreira <82904762+victorfyonemoto@users.noreply.github.com>
2022-05-03 14:56:00 -0300

    Add files via upload
    first commit

---
+++ Java/desafioBanco/src/cliente.java
@@ -0,0 +1,24 @@
+public class cliente {
+
+    public String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String CPF;
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+
+}
